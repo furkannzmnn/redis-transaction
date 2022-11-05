172d2e36696481c33b0e9929690b213a10e652a0
Furkan Özmen <31289019+furkannzmnn@users.noreply.github.com>
2022-11-05 11:16:00 +0300

    redis rollback

--- src/main/java/com/example/redistransaction/BalanceTransfer.java
+++ src/main/java/com/example/redistransaction/BalanceTransfer.java
@@ -6,6 +6,7 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.SessionCallback;
+import org.springframework.data.redis.core.ValueOperations;
 
 import java.util.List;
 import java.util.Objects;
--- src/main/java/com/example/redistransaction/RedisTransactionApplication.java
+++ src/main/java/com/example/redistransaction/RedisTransactionApplication.java
@@ -4,7 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SessionCallback;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+
+import java.util.List;
 
 @SpringBootApplication
 public class RedisTransactionApplication implements CommandLineRunner {
@@ -27,7 +33,7 @@ public class RedisTransactionApplication implements CommandLineRunner {
 
         var balanceTransfer = BalanceTransfer.of(1, 2, 10);
         try {
-            redisTemplate.execute(balanceTransfer);
+            redisTemplate.execute(new RedisRollback());
         }catch (Exception e) {
             // TRANSACTION ROLLBACK
             System.out.println("account1 = " + hashOperations.get("account:1"));
@@ -37,5 +43,15 @@ public class RedisTransactionApplication implements CommandLineRunner {
         System.out.println("account1 = " + hashOperations.get("account:1"));
         System.out.println("account2 = " + hashOperations.get("account:2"));
         // TRANSACTION
+    }
+}
+class RedisRollback implements SessionCallback<List<Object>> {
+
+    @Override
+    public <K, V> List<Object> execute(RedisOperations<K, V> operations) throws DataAccessException {
+        operations.multi();
+        operations.discard();
+        System.out.println("RedisRollback.execute");
+        return operations.exec();
     }
 }
